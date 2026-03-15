0376f7d7637d366d91cb824e6a93696de557a55a
Hyeonbinyoon <nickhon77@gmail.com>
2026-03-15 18:51:46 +0900

    Tested big-endian, small-endian by try.cpp

---
+++ try.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <netinet/in.h> 
+
+int main(int argc, char *argv[]) {
+	uint32_t numbers[10];
+	uint8_t network_buffer[40];
+	if (argc < 2) {
+		printf("File not found");
+		return 1;
+	}
+	
+	for (int i = 1; i <argc; i++){
+		FILE *fp = fopen(argv[i], "rb");
+
+		if (fp == NULL) {
+			printf("fopen (no.%d file): No such file or directoy",i);
+			return 1;
+		}
+
+	/*version1
+	size_t file_size = 0;
+	
+	while (fread((network_buffer) + 4*(i-1) + file_size ,1,1,fp) == 1) {
+		file_size ++;}
+
+	if(file_size<4) {printf("File is smaller than 4byte"); return 1;}
+	if(file_size>4) {printf("File is bigger than 4byte"); return 1;}
+	
+	fclose(fp);
+	}
+
+
+	for(int i = 0; i< 4 * (argc - 1); i++) {
+		printf("%02x" , network_buffer[i]);
+		if(i%4 == 3) printf("\n");
+	}
+
+	return 0;
+	} */
+
+	
+
+	//version2
+	fseek(fp, 0, SEEK_END);
+	long file_size = ftell(fp);
+	rewind(fp);   
+
+	if(file_size<4) {printf("File is smaller than 4byte"); return 1;}
+        if(file_size>4) {printf("File is bigger than 4byte"); return 1;}
+	
+	fread(numbers + i, 4, 1, fp);
+	printf("before ntohs: %d(0x%08x)\n", numbers[i],numbers[i]);
+	printf("after ntohs: %d(0x%08x)\n", ntohl(numbers[i]), ntohl(numbers[i]));
+	fclose(fp); }
+
+	return 0; }
+
+
