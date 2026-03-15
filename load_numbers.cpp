a092684dbbdc5ba8f0fb583196de3ee2295c1d8e
Hyeonbinyoon <nickhon77@gmail.com>
2026-03-15 22:52:53 +0900

    Add a newline to the error message

--- load_numbers.cpp
+++ load_numbers.cpp
@@ -6,17 +6,17 @@
 int load_numbers(int i, FILE *filepointer, uint32_t *number){
 	uint32_t box;
 		
-	if(fseek(filepointer, 0, SEEK_END) != 0) {printf("File%d seeking error", i); return 0;}
+	if(fseek(filepointer, 0, SEEK_END) != 0) {printf("File%d seeking error\n", i); return 0;}
 
         long file_size = ftell(filepointer);
-	if(file_size  == -1) {printf("File%d telling error", i); return 0;}
+	if(file_size  == -1) {printf("File%d telling error\n", i); return 0;}
         
 	rewind(filepointer);
 
-        if(file_size<4) {printf("File%d is smaller than 4byte", i); return 0;}
-        if(file_size>4) {printf("File%d is bigger than 4byte", i); return 0;}
+        if(file_size<4) {printf("File%d is smaller than 4byte\n", i); return 0;}
+        if(file_size>4) {printf("File%d is bigger than 4byte\n", i); return 0;}
 
-	if(fread(&box, 4, 1, filepointer) == 0) {printf("File%d reading error", i); return 0;}
+	if(fread(&box, 4, 1, filepointer) == 0) {printf("File%d reading error\n", i); return 0;}
 	*number = ntohl(box);
 
 	return 1;
